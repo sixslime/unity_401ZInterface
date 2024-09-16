b27b2fcb421d174efa286834a27afccab594f1bb
GFunction <ryan.mm.taylor@gmail.com>
2024-09-16 14:23:26 -0700

    added controlflows

---
+++ Assets/Scripts/Util/{ControlledFlows}/Extensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlledFlows;
+public static class Extensions
+{
+    public static TransformedFlow<I, R> WithTransformedResult<I, R>(this ICeasableFlow<I> inputFlow, Func<I, R> transform)
+    {
+        return new(inputFlow, transform);
+    }
+}
